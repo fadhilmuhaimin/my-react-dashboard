048e0a58f6d74f868dab742d4f9f96afa13c3c88
fadhilmuhaimin <fadilmuhaimin88@gmail.com>
2024-12-15 01:38:11 +0800

    Add Profile Page

--- src/pages/profile/ProfilePage.jsx
+++ src/pages/profile/ProfilePage.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import { Descriptions, Avatar, Card } from 'antd';
+import { useAuthStore } from '../../store/authStore';
 
 const ProfilePage = () => {
-    return(
-        <>
-            
-        </>
-    )
-}
+  const { user } = useAuthStore();
+  if(!user) return null;
+
+  return (
+    <Card>
+      <Avatar src={user.image} size={64} style={{marginBottom:'20px'}}/>
+      <Descriptions bordered title="User Info">
+        <Descriptions.Item label="ID">{user.id}</Descriptions.Item>
+        <Descriptions.Item label="Username">{user.username}</Descriptions.Item>
+        <Descriptions.Item label="Email">{user.email}</Descriptions.Item>
+        <Descriptions.Item label="First Name">{user.firstName}</Descriptions.Item>
+        <Descriptions.Item label="Last Name">{user.lastName}</Descriptions.Item>
+        <Descriptions.Item label="Gender">{user.gender}</Descriptions.Item>
+      </Descriptions>
+    </Card>
+  );
+};
+
 export default ProfilePage;
